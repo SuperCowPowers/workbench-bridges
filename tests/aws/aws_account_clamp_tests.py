df13bf1c3c49e7d50c690b6ead6f32ee53da97a7
Brian Wylie <briford.wylie@gmail.com>
2024-10-19 22:22:09 -0600

    initial SageWorks Bridges repo setup

---
+++ tests/aws/aws_account_clamp_tests.py
@@ -0,0 +1,19 @@
+"""Tests for the AWS Account Stuff"""
+
+# SageWorks-Bridge Imports
+from sageworks_bridges.aws.sm_session import get_sagemaker_session
+
+
+def test_sm_session():
+    """Tests for the AWS Account Stuff"""
+
+    # Get SageMaker Session
+    sm_session = get_sagemaker_session()
+
+    # List SageMaker Models
+    print("\nSageMaker Models:")
+    sagemaker_client = sm_session.sagemaker_client
+    response = sagemaker_client.list_models()
+
+    for model in response["Models"]:
+        print(model["ModelName"])
